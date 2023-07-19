7a947c4bc7b8e836ff5a8d305a91698718d6229f
Pawel Goliszewski <pawlik9.hamster@gmail.com>
2023-07-19 13:40:28 +0200

    feat: added solutions for oop block

--- 5_oop/solutions/1_additional_operation.py
+++ 5_oop/solutions/1_additional_operation.py
@@ -6,6 +6,13 @@ class MyVector(Vector):
     def __neg__(self):
         return self.__class__(*[-x for x in self.values])
 
+    # Alternatywne rozwiązanie:
+    # def __neg__(self):
+    #     result = MyVector()
+    #     result.values = tuple(-x for x in self.values)
+    #     return result
+
+
 
 vector: MyVector = MyVector() + 1
 assert (-vector).values == MyVector(-1, -1).values
