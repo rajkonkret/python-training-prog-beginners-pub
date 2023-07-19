b696fa86741a5caf8738940cc184b171edd90132
Pawel Goliszewski <pawlik9.hamster@gmail.com>
2023-07-19 13:45:12 +0200

    feat: added solutions for oop block

--- 5_oop/solutions/2_personprinter.py
+++ 5_oop/solutions/2_personprinter.py
@@ -1,6 +1,7 @@
 # Uzupełnij klasę Person tak, aby kod wykonywał się zgodnie z komentarzami
 
 import datetime
+from typing import List
 
 
 class Person:
@@ -20,6 +21,25 @@ class Person:
     def from_string(string: str) -> 'Person':
         name, surname, age = string.split(',')
         return Person(name, surname, int(age))
+
+# Alternatywne rozwiązanie
+# class Person:
+#     def __init__(self, name: str, surname: str, age: int):
+#         self.name = name
+#         self.surname = surname
+#         self.age = age
+#         self.year_of_birth = datetime.date.today().year - self.age
+#
+#     def __str__(self):
+#         return f"{self.name} {self.surname}, {self.age}"
+#
+#     @staticmethod
+#     def from_string(string: str) -> 'Person':
+#         split_string: list[str] = string.split(',')
+#         name: str = split_string[0]
+#         surname: str = split_string[1]
+#         age: int = int(split_string[2])
+#         return Person(name, surname, age)
 
 
 data: list[str] = ["Jacek,Kowalski,18", "Anna,Nowak,35"]
